2ae47e9e6ee5d5e59dc3f596333312506ecbe5ee
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-10-17 00:28:55 -1000

    feat: insert multiple sample records into lowercase customer table

---
+++ task_6.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store; 
+
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DECIMAL(10,2),
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+SHOW TABLES;
+INSERT INTO customer(customer_id, customer_name, email, address) 
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+        (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.')
+        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.')
+        (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
